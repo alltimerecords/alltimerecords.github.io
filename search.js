a506691e6b79d06ef501394bb3e8d3b3f8f64b6e
alltimerecords <54764920+alltimerecords@users.noreply.github.com>
2019-09-01 21:11:44 +1200

    Add files via upload

---
+++ search.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$('.autosuggest').keyup(function(){var search_term = $(this).attr('value');$.post('ajax/search.php', {search_term:search_term}, function(data){$('.result').html(data);$('.result li').click(function(){var result_value = $(this).text();$('.autosuggest').attr('value', result_value);$('.result').html('');});});});});
